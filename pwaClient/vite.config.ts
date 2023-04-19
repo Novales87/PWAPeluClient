872cf811b8ff9fdfddf2f74845144ef0ca52cdd3
Juan Fierro <juan.fierro@mediamonks.com>
2023-04-19 17:20:34 -0300

    implementacion de Typescript

---
+++ pwaClient/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig, Plugin } from 'vite';
+import react from '@vitejs/plugin-react';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const cssPlugin = (): Plugin => {
+  return {
+    name: 'css-transform',
+    transform(src, id) {
+      if (/\.(scss)$/.test(id)) {
+        return src.replace(/%(.*?)%/g, (match, p1) => process.env[p1] || '');
+      }
+    }
+  };
+};
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), cssPlugin()],
+});
